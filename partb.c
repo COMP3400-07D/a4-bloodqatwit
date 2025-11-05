e296225803e1c60d34d446d0fdf66474867e4ac0
Quinn Blood <bloodq@wit.edu>
2025-11-04 20:44:04 -0500

    Complete

--- partb.c
+++ partb.c
@@ -1,8 +1,35 @@
 #include <stdbool.h>
 #include <stddef.h>
+#include <unistd.h>
 
 // TODO: Include more header files (read the manpages to find out more!)
 
 int main(int argc, const char* argv[]) {
+
+    if (argc < 2) {
+        printf("ERROR: no arguments\n");
+        return 1;
+    }
+
+    int mid = argc / 2;
+    char *eargs[4];
+
+    eargs[0] = "echo";
+
+    if ((argc - 1) % 2 == 0) {
+        eargs[1] = argv[mid - 1];
+        eargs[2] = argv[mid];
+        eargs[3] = NULL;
+    } else {
+        eargs[1] = argv[mid];
+        eargs[2] = NULL;
+    }
+
+    int ret = execv("/bin/echo", eargs);
+    if (ret == -1) {
+        perror("execv");
+        return 1;
+    }
+
     return 0;
 }
--- partc.c
+++ partc.c
@@ -1,9 +1,50 @@
 #include <stdbool.h>
 #include <stddef.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
 
 // TODO: Include more header files (read the manpages to find out more!)
 
 int main(int argc, const char* argv[]) {
-    // TODO: Complete and document
+    if (argc != 3) {
+        printf("ERROR: no arguments\n");
+        return 1;
+    }
+
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror("fork");
+        return 1;
+    }
+
+    if (pid == 0) {
+        // Child: run grep
+        char *args[] = { "grep", "-s", "-q", argv[1], argv[2], NULL };
+        execv("/usr/bin/grep", args);
+        perror("execv");
+        exit(1);
+    } else {
+        int status;
+        wait(&status);
+
+        if (WIFEXITED(status)) {
+            int exit_code = WEXITSTATUS(status);
+            if (exit_code == 0) {
+                printf("FOUND: %s\n", argv[1]);
+                return 0;
+            } else if (exit_code == 1) {
+                printf("NOT FOUND: %s\n", argv[1]);
+                return 0;
+            } else if (exit_code == 2) {
+                printf("ERROR: %s doesn't exist\n", argv[2]);
+                return 2;
+            }
+        } else {
+            printf("ERROR: grep failed\n");
+            return 2;
+        }
+    }
+
     return 0;
 }
